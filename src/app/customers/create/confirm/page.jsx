96e0e4927b499d170d23dfe064f5dd087cccf488
TAS-11 <cm40021@gmail.com>
2025-12-18 14:38:14 +0900

    Update page.jsx

--- src/app/customers/create/confirm/page.jsx
+++ src/app/customers/create/confirm/page.jsx
@@ -1,9 +1,8 @@
+"use client";
 import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
 import fetchCustomer from "./fetchCustomer";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-
-"use client";
 import { Suspense } from "react";
 function ConfirmPageContent() {
   const customer_id = useSearchParams().get("customer_id");
