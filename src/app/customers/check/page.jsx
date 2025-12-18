2b27a231684892591bd1b6179b2e0336cf99dcd6
TAS-11 <cm40021@gmail.com>
2025-12-18 12:59:31 +0900

    first commit

---
+++ src/app/customers/check/page.jsx
@@ -0,0 +1,35 @@
+import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
+
+export const dynamic = 'force-dynamic';
+
+async function fetchCustomer(id) {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch customer");
+  }
+  return res.json();
+}
+
+function ReadPage({ searchParams }) {
+  const id = searchParams?.id;  if (!id) {
+    return (
+      <div className="alert alert-error">        
+      IDが指定されていません
+      </div>    );  }
+
+  const customerInfo = await fetchCustomer(id);
+
+  return (
+    <>
+      <div className="alert alert-success">更新しました</div>
+      <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
+        <OneCustomerInfoCard {...customerInfo[0]} />
+      </div>
+      <button className="btn btn-outline btn-accent">
+        <a href="/customers">一覧に戻る</a>
+      </button>
+    </>
+  );
+}
---
+++ src/app/customers/create/confirm/page.jsx
@@ -0,0 +1,43 @@
+import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
+import fetchCustomer from "./fetchCustomer";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+"use client";
+import { Suspense } from "react";
+function ConfirmPageContent() {
+  const customer_id = useSearchParams().get("customer_id");
+    // ...
+  }
+export default function ConfirmPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+          <ConfirmPageContent />    
+    </Suspense>
+  );
+ }
+  const router = useRouter();
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    const fetchAndSetCustomer = async () => {
+      const customerData = await fetchCustomer(customer_id);
+      setCustomer(customerData);
+    };
+    fetchAndSetCustomer();
+  }, []);
+
+  return (
+    <>
+      <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
+        <div className="alert alert-success p-4 text-center">
+          正常に作成しました
+        </div>
+        <OneCustomerInfoCard {...customer} />
+        <button onClick={() => router.push("./../../customers")}>
+          <div className="btn btn-primary m-4 text-2xl">戻る</div>
+        </button>
+      </div>
+    </>
+  );
+}
